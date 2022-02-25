88217325ac6da90397bc149c644b1657dedc7fb9
AliTaarek <eng.alii.tarek@gmail.com>
2022-02-25 21:04:36 +0200

    test commit ali

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
