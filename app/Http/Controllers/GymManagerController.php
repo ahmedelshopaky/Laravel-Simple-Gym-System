bb2806a2ca82377f843cb0230935e3da0c962516
Asmaa100 <asmaaebrahim11298@gmail.com>
2022-03-01 17:36:24 +0200

    Edit

--- app/Http/Controllers/GymManagerController.php
+++ app/Http/Controllers/GymManagerController.php
@@ -13,8 +13,8 @@ class GymManagerController extends Controller
     
     public function index(Request $request){
         if ($request->ajax()) {
-            $data = GymManager::where('id','1')->get();
-            dd($data);
+            $data = GymManager::first()->get();
+            // dd($data);
             return Datatables::of($data)->addIndexColumn()
                     ->addColumn('action', function($row){
                            $Btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm mr-3">Edit</a>';
