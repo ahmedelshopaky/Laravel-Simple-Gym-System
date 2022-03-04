569fc8b2809d3eea4f3fa411d97d5676d0aaed3c
AliTaarek <eng.alii.tarek@gmail.com>
2022-03-04 02:11:22 +0200

    api progress

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register',[AuthController::class,'register']);
+
+Route::post('login',[AuthController::class,'login']);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
 
 Route::middleware('auth:sanctum')->group(function(){
 
