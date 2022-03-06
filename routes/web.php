7fdd1580f5f1d851c4a3e096ab00dc132be497fe
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-03-06 12:56:27 +0200

    roles are now working on routes, create AdminSeeder

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
     
-    Route::group(['prefix'=>'/gym-managers','middleware' => 'auth', 'role:admin|cityManager'], function () {
+    Route::group(['prefix'=>'/gym-managers','middleware' => 'role:admin|cityManager'], function () {
         Route::get('/', [GymManagerController::class, 'index'])->name('gym-managers.index');
         Route::get('/create', [GymManagerController::class, 'create'])->name('gym-managers.create');
         Route::get('/{id}/edit', [GymManagerController::class, 'edit'])->name('gym-managers.edit');
@@ -44,7 +44,7 @@ Route::middleware('auth')->group(function () {
         Route::put('/{id}/unban', [GymManagerController::class, 'unban'])->name('gym-managers.unban');
     });
 
-    Route::group(['prefix'=>'/city-managers','middleware' => 'auth', 'forbid-banned-user','role:admin'], function () {
+    Route::group(['prefix'=>'/city-managers','middleware' => ['forbid-banned-user','role:admin']], function () {
         Route::get('/', [CityManagerController::class, 'index'])->name('city-managers.index');
         Route::get('/create', [CityManagerController::class, 'create'])->name('city-managers.create');
         Route::get('/{id}/edit', [CityManagerController::class, 'edit'])->name('city-managers.edit');
@@ -59,7 +59,7 @@ Route::middleware('auth')->group(function () {
     });
 
     
-    Route::group(['prefix' => '/users', 'middleware' => 'auth','forbid-banned-user','role:admin|cityManager|gymManager'], function () {
+    Route::group(['prefix' => '/users', 'middleware' => ['forbid-banned-user','role:admin|cityManager|gymManager']], function () {
         Route::get('/', [UserController::class, 'index'])->name('users.index');
         Route::get('/create', [UserController::class, 'create'])->name('users.create');
         Route::post('/', [UserController::class, 'store'])->name('users.store');
@@ -74,7 +74,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/', [CityController::class, 'index'])->name('cities.index');
     });
     
-    Route::group(['prefix' => '/gyms', 'middleware' => 'auth', 'forbid-banned-user','role:admin|cityManager'], function () {
+    Route::group(['prefix' => '/gyms', 'middleware' => ['forbid-banned-user','role:admin|cityManager']], function () {
         Route::get('/', [GymController::class, 'index'])->name('gyms.index');
         Route::get('/create', [GymController::class,'create'])->name('gyms.create');
         Route::post('/', [GymController::class,'store'])->name('gyms.store');
@@ -86,6 +86,7 @@ Route::middleware('auth')->group(function () {
     });
 
     Route::get('/training-packages', [TrainingPackageController::class, 'index'])->name('training-packages.index');
+
     Route::prefix('/coaches')->group(function () {
         Route::get('/', [CoachController::class, 'index'])->name('coaches.index');
         Route::get('/create', [CoachController::class, 'create'])->name('coaches.create');
@@ -94,7 +95,7 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
 
-    Route::group(['prefix' => '/buy-package', 'middleware' => 'auth','forbid-banned-user','role:admin'], function () {
+    Route::group(['prefix' => '/buy-package', 'middleware' => ['forbid-banned-user','role:admin']], function () {
         Route::get('/create', [BuyPackageController::class, 'create'])->name('buy-package.create');
         Route::post('/', [BuyPackageController::class, 'store'])->name('buy-package.store');
     });
@@ -102,7 +103,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/revenue', [RevenueController::class, 'index'])->name('revenue.index');
 
     
-    Route::group(['prefix' => '/training-sessions', 'middleware' => 'auth','forbid-banned-user','role:admin|cityManager|gymManager'], function () {
+    Route::group(['prefix' => '/training-sessions', 'middleware' => ['forbid-banned-user','role:admin|cityManager|gymManager']], function () {
         Route::prefix('/revenue')->group(function () {
             Route::get('/', [RevenueController::class, 'index'])->name('revenue.index');
         });
