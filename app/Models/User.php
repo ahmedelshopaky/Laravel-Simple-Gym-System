398f588f169f34d221c68f1ec9a4978bdb28afea
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-03-06 21:31:52 +0200

    ROLES ARE IN ACTION ;D

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,9 +61,4 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasOne(GymMember::class, 'user_id');
     }
-
-    public function role()
-    {
-        return $this->morphTo();
-    }
 }
