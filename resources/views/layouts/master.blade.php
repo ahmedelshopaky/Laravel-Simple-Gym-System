2944fa82ffafe2aae9b63dad30d10d5e00c8edae
abdeladlshaheen421 <abdeladlshaheen@gmail.com>
2022-03-10 22:40:15 +0200

    link sweet alert Successfully

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,6 +19,7 @@
 
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js">
     </script>
+    @require("vendor\almasaeed2010\adminlte\plugins\sweetalert2\sweetalert2.min.js")
     @yield('styles')
 </head>
 
