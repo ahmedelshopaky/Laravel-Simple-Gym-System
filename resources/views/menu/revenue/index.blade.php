ed5a89cbbf30547812dfa3865e0b94964c8d4195
Asmaa100 <asmaaebrahim11298@gmail.com>
2022-03-11 12:28:52 +0200

    Edit Total Revenue

--- resources/views/menu/revenue/index.blade.php
+++ resources/views/menu/revenue/index.blade.php
@@ -2,30 +2,30 @@
 @section('content')
 
 <section class="content mt-5">
-  <div class="container-fluid">
+  <!-- <div class="container-fluid">
     <div class="row">
       <div class="col-md-4 ">
 
         <div class="card">
           <div class="card-header">
             @role('admin')
-            <h3 class="card-title py-3 text-white fs-4">Total Revenue</h3> <!-- ADMIN ONLY -->
+            <h4 class="card-title h4y-3 text-white fs-4">Total Revenue</h4> 
             @endrole
             @role('cityManager')
-            <h3 class="card-title">Total Revenue For Your City</h3> <!-- CITY MANAGER ONLY -->
+            <h4 class="card-title">Total Revenue For Your City</h4> 
             @endrole
             @role('gymManager')
-            <h3 class="card-title">Total Revenue For Your Gym</h3> <!-- GYM MANAGER ONLY -->
+            <h4 class="card-title">Total Revenue For Your Gym</h4> 
             @endrole
             <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <button tyh4e="button" class="btn btn-tool" data-card-widget="collah4se" title="Collah4se">
                 <i class="fas fa-minus text-white"></i>
               </button>
             </div>
           </div>
 
           <div class="card-body">
-            <div class="card-body pt-0">
+            <div class="card-body h4t-0">
               <div class="row">
                 <div class="col-7">
                   <h2 class="lead"><b>Total Revenue = ${{$totalRevenue / 100}}</b></h2>
@@ -34,38 +34,177 @@
             </div>
           </div>
         </div>
+        
 
       </div>
-    </div>
-  </div>
-
-  <!-- --------------------------------------------------------- -->
-  <!-- --------------------------------------------------------- -->
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
+      <div class="col-md-4 ">
 
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title py-3 text-white fs-4">Purchases History</h3>
+            @role('admin')
+            <h4 class="card-title h4y-3 text-white fs-4">Total Revenue</h4> 
+            @endrole
+            @role('cityManager')
+            <h4 class="card-title">Total Revenue For Your City</h4>
+            @endrole
+            @role('gymManager')
+            <h4 class="card-title">Total Revenue For Your Gym</h4> 
+            @endrole
             <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <button tyh4e="button" class="btn btn-tool" data-card-widget="collah4se" title="Collah4se">
                 <i class="fas fa-minus text-white"></i>
               </button>
             </div>
           </div>
 
           <div class="card-body">
-            <div class="card-body pt-0">
+            <div class="card-body h4t-0">
+              <div class="row">
+                <div class="col-7">
+                  <h2 class="lead"><b>Total Revenue = ${{$totalRevenue / 100}}</b></h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+
+      </div>
+      <div class="col-md-4 ">
+
+        <div class="card">
+          <div class="card-header">
+            @role('admin')
+            <h4 class="card-title h4y-3 text-white fs-4">Total Revenue</h4> 
+            @endrole
+            @role('cityManager')
+            <h4 class="card-title">Total Revenue For Your City</h4> 
+            @endrole
+            @role('gymManager')
+            <h4 class="card-title">Total Revenue For Your Gym</h4> 
+            @endrole
+            <div class="card-tools">
+              <button tyh4e="button" class="btn btn-tool" data-card-widget="collah4se" title="Collah4se">
+                <i class="fas fa-minus text-white"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="card-body h4t-0">
+              <div class="row">
+                <div class="col-7">
+                  <h2 class="lead"><b>Total Revenue = ${{$totalRevenue / 100}}</b></h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+
+      </div>
+    </div>
+  </div> -->
+
+  <!-- --------------------------------------------------------- -->
+  <!-- --------------------------------------------------------- -->
+
+  <div class="container">
+        <div class="row justify-content-center">
+           
+            <div class="col-lg-4 col-6">
+
+                <div class="small-box  info text-white">
+                    <div class="inner">
+                    @role('admin')
+                      <h4 class="">Total Revenue</h4> 
+                      @endrole
+                      @role('cityManager')
+                      <h4 class="">Total Revenue For Your City</h4> 
+                      @endrole
+                      @role('gymManager')
+                      <h4 class="">Total Revenue For Your Gym</h4> 
+                      @endrole
+                        <h3>${{$totalRevenue / 100}}</h3>
+                        
+                    </div>
+                    <div class="icon">
+                        <i class="far fa-usd nav-icon text-white"></i>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4 col-6">
+
+                <div class="small-box info  text-white">
+                <div class="inner">
+                    @role('admin')
+                      <h4 class="">Total Revenue</h4> 
+                      @endrole
+                      @role('cityManager')
+                      <h4 class="">Total Revenue For Your City</h4> 
+                      @endrole
+                      @role('gymManager')
+                      <h4 class="">Total Revenue For Your Gym</h4> 
+                      @endrole
+                        <h3>${{$totalRevenue / 100}}</h3>
+                        
+                    </div>
+                    <div class="icon">
+                        <i class="far fa-usd nav-icon text-white"></i>
+                    </div>
+                   
+                </div>
+            </div>
+            
+            <div class="col-lg-4 col-6">
+
+                <div class="small-box info text-white">
+                    <div class="inner">
+                    @role('admin')
+                      <h4 class="">Total Revenue</h4> 
+                      @endrole
+                      @role('cityManager')
+                      <h4 class="">Total Revenue For Your City</h4> 
+                      @endrole
+                      @role('gymManager')
+                      <h4 class="">Total Revenue For Your Gym</h4> 
+                      @endrole
+                        <h3>${{$totalRevenue / 100}}</h3>
+                        
+                    </div>
+                    <div class="icon">
+                        <i class="far fa-usd nav-icon text-white"></i>
+                    </div>
+                   
+                </div>
+            </div>
+
+
+        </div>
+    <div class="row">
+      <div class="col-12">
+
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title h4y-3 text-white fs-4">h4urchases History</h4>
+            <div class="card-tools">
+              <button tyh4e="button" class="btn btn-tool" data-card-widget="collah4se" title="Collah4se">
+                <i class="fas fa-minus text-white"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="card-body h4t-0">
               <table class="table table-bordered table-hover yajra-datatable data-table" id="">
                 <thead>
                   <tr>
                     <!-- <th>ID</th> -->
                     <th>Client Name</th>
                     <th>Email</th>
-                    <th>Package Name</th>
-                    <th>Amount paid</th>
+                    <th>h4ackage Name</th>
+                    <th>Amount h4aid</th>
                     @hasanyrole('admin|cityManager')
                     <th>GYM</th> <!-- CITY MANAGER AND ADMIN ONLY -->
                     @endhasanyrole
@@ -81,9 +220,9 @@
             </div>
           </div>
         </div>
+        </div>
 
       </div>
-    </div>
   </div>
 </section>
 
@@ -112,8 +251,8 @@
           name: 'training_package_name'
         },
         {
-          data: 'amount_paid',
-          name: 'amount_paid'
+          data: 'amount_h4aid',
+          name: 'amount_h4aid'
         },
         {
           data: 'gym_name', // <!-- CITY MANAGER AND ADMIN ONLY -->
@@ -153,8 +292,8 @@
           name: 'training_package_name'
         },
         {
-          data: 'amount_paid',
-          name: 'amount_paid'
+          data: 'amount_h4aid',
+          name: 'amount_h4aid'
         },
         {
           data: 'gym_name', // <!-- CITY MANAGER AND ADMIN ONLY -->
@@ -166,15 +305,15 @@
 </script>
 @endrole
 @role('gymManager')
-<script>
+<scrih4t>
   $(function() {
-    $.ajaxSetup({
+    $.ajaxSetuh4({
       headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
       }
     });
     var dataTable = $('.data-table').DataTable({
-      processing: true,
+      h4rocessing: true,
       serverSide: true,
       ajax: "{{ route('revenue.index') }}",
       columns: [{
@@ -186,16 +325,16 @@
           name: 'gym_member_email'
         },
         {
-          data: 'training_package_name',
-          name: 'training_package_name'
+          data: 'training_h4ackage_name',
+          name: 'training_h4ackage_name'
         },
         {
-          data: 'amount_paid',
-          name: 'amount_paid'
+          data: 'amount_h4aid',
+          name: 'amount_h4aid'
         },
       ]
     });
   });
-</script>
+</scrih4t>
 @endrole
 @endsection
