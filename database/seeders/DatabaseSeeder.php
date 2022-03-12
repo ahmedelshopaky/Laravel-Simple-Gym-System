8ebdf2d2891d49c1f8253781307567ede62ed222
ahmedelshopaky <engelshopaky@gmail.com>
2022-03-12 04:39:32 +0200

    some magic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,17 +22,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //php artisan migrate:fresh --seed
-
-        // $numberOfUsers=300;
-        // // User::factory(10)->create();
-        // GymMember::factory(10)->create();
-        // CityManager::factory(10)->create();
-        // // Gym::factory(5)->create();
-        // Coach::factory(5)->create();
-        // GymManager::factory(5)->create();
-        // TrainingPackage::factory(10)->create();
-        // TrainingSession::factory(10)->create();
+        // User::factory(10)->create();
+        GymMember::factory(10)->create();
+        CityManager::factory(10)->create();
+        // Gym::factory(5)->create();
+        Coach::factory(5)->create();
+        GymManager::factory(5)->create();
+        TrainingPackage::factory(10)->create();
+        TrainingSession::factory(10)->create();
 
         $this->call(PermissionsSeeder::class);
     }
